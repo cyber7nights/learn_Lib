dfb069ff27891322df40705187e6e42092e897be
XMR <x_93@outlook.com>
2017-07-10 16:54:21 +0800

    fist time to practice urllib

--- urllib/learning.py
+++ urllib/learning.py
@@ -1,2 +1,9 @@
 # -*- coding=utf-8 -*-
-import urllib
+import urllib.request  #Python3.x中，将urllib与urllib2合并为一起。urllib.request 就是 urllib2
+#from urllib import request 时，下面代码用request.urlopen()
+with urllib.request.urlopen('https://www.baidu.com') as f:
+    data = f.read()
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', data.decode('utf-8'))
