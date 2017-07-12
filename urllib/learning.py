4049d38324d7e8e56a72206ebda7ae33e3a09d44
XMR <x_93@outlook.com>
2017-07-12 16:45:45 +0800

    new learning user-agent

--- urllib/learning.py
+++ urllib/learning.py
@@ -1,5 +1,6 @@
 # -*- coding=utf-8 -*-
 import urllib.request
+import urllib.parse
 #Python3.x中，将urllib与urllib2合并为一起。urllib.request 就是 urllib2
 #from urllib import request 时，下面代码用request.urlopen()
 #  with urllib.request.urlopen('https://www.baidu.com') as f:
@@ -8,13 +9,24 @@ import urllib.request
 #     for k, v in f.getheaders():
 #         print('%s: %s' % (k, v))
 #     print('Data:', data.decode('utf-8'))
-url=input('please enter url (默认：https://www.baidu.com):')
+url=input('please enter url (默认：https://www.zhihu.com/people/wo-jiu-shi-yue-xia/activities):')
 if len(url)<1:
-    url='https://www.baidu.com'
+    url='https://www.zhihu.com/people/wo-jiu-shi-yue-xia/activities'
+user_agent='Mozilla/5.0 (Windows NT 6.1)'
+headers={'User-Agent':user_agent,
+         'Referer':'https://www.zhihu.com/people/wo-jiu-shi-yue-xia/activities'}
 
-with urllib.request.urlopen(url) as f:
-    data=f.read()
-    print(data.decode('utf-8'))
-    print('info():\n',f.info())
+# values={'username':'******','password':'*****'}
+# data=urllib.parse.urlencode(values)
+# data=data.encode('ascii')
+
+req=urllib.request.Request(url=url,headers=headers)
+
+with urllib.request.urlopen(req) as f:
+    response=f.read()
+    print(response.decode('utf-8'))
+    # print('info():\n',f.info())
+
     print('getcode():\n',f.getcode())
     print('geturl():\n',f.geturl())
+
